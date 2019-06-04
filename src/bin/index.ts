abda8a4487d6abadab8b291bbbc957baeaf8d88c
Juste Méthode B <justkeybocovo@gmail.com>
2019-06-04 07:31:17 +0100

    adding API and watching file changes

---
+++ src/bin/index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env nod
+import * as program from 'commander';
+import { replaceTscAliasPaths } from '..';
+program
+  .name('tsc-alias')
+  .version('1.1.0')
+  .option('-p, --project <file>', 'path to tsconfig.json')
+  .option('-w, --watch', 'Observe file changes')
+  .option(
+    '--dir, --directory <dir>',
+    'Run in a folder leaving the "outDir" of the tsconfig.json (relative path to tsconfig)'
+  )
+  .parse(process.argv);
+
+replaceTscAliasPaths({
+  configFile: program.project,
+  watch: !!program.watch,
+  outDir: program.directory
+});
