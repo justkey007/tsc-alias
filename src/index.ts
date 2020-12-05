eb3319f2c2fd2f13df2da84c86664a0387016b54
Lawrence Wagerfield <lawrence@projectone.io>
2020-12-05 18:02:07 +0000

    Support empty relative alias paths

--- src/index.ts
+++ src/index.ts
@@ -180,7 +180,12 @@ export function replaceTscAliasPaths(
       );
 
       if (relativeAliasPath[0] !== '.') {
-        relativeAliasPath = './' + relativeAliasPath;
+        if (relativeAliasPath.length === 0) {
+          relativeAliasPath = '.'
+        }
+        else {
+          relativeAliasPath = './' + relativeAliasPath;
+        }
       }
 
       const modulePath =
