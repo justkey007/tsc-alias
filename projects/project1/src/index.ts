fba00da328275076990d9c2b35fe5f8f437931ad
Juste Méthode B <justebocovo@gmail.com>
2021-03-25 15:01:18 +0100

    fix: #21

--- projects/project1/src/index.ts
+++ projects/project1/src/index.ts
@@ -3,6 +3,8 @@ import { CUSTOM_MODULE } from 'custom/index';
 import { EXTRA } from 'extra';
 import { add } from 'myproject/custom_modules/calculator';
 
+export { DATA as XYZ } from '@commons';
+
 console.log(DATA);
 console.log(CUSTOM_MODULE);
 console.log(EXTRA);
