26b69d4cc2c4547f39f2f0f11b19f75fdb13d818
Adam Coster <adam@bscotch.net>
2021-06-06 09:22:55 -0500

    fix: Ensured that errors crash the process even when in 'silent' mode, and that the exit code is non-zero.
    Closes justkey007/tsc-alias#32

--- src/utils/output.ts
+++ src/utils/output.ts
@@ -3,14 +3,25 @@ import { Output as Console } from '@jfonx/console-utils';
 export class Output {
   constructor(private silent = false) {}
 
+  /**
+   * Crash the process with a non-zero exit code
+   */
+  private static exitProcessWithError() {
+    process.exit(1);
+  }
+
   info(message: string) {
     if (this.silent) return;
     Console.info(message);
   }
 
-  error(message: string, exitProces = false) {
-    if (this.silent) return;
-    Console.error(message, exitProces);
+  error(message: string, exitProcess = false) {
+    if (!this.silent) {
+      Console.error(message);
+    }
+    if (exitProcess) {
+      Output.exitProcessWithError();
+    }
   }
 
   clear() {
