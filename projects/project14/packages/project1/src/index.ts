6a0abc7527b0643481065fa41faf9a5ad1ff29fe
Raoul de Heer <raouldeheer@users.noreply.github.com>
2021-10-03 15:48:15 +0200

    Update test to npm workspace

--- projects/project14/packages/project1/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { DATA } from '@commons';
-import { CUSTOM_MODULE } from 'custom/index';
-import { add } from 'myproject/custom_modules/calculator';
-
-export { DATA as XYZ } from '@commons';
-
-console.log(DATA);
-console.log(CUSTOM_MODULE);
-console.log(add(1, 2));
