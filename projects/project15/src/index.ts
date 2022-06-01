1499375a2090fdde71aecd51a0d2c404675d5cbe
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-06-01 19:07:35 +0200

    Added module resolver with test

--- projects/project15/src/index.ts
+++ projects/project15/src/index.ts
@@ -1,6 +1,10 @@
 import { Controller } from '@controllers/index';
 import { Router } from '@router';
 
+declare module '@router' {
+    export class Router2 { }
+}
+
 new Controller();
 
 new Router();
