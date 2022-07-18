847f9456e2787afa7e1177382ae9a81364ad0aef
Juste Méthode B <36489637+justkey007@users.noreply.github.com>
2022-07-18 13:41:30 +0200

    fix: reference in node_modules folder (#132)
    Fixes #130

--- projects/project9/src/index.ts
+++ projects/project9/src/index.ts
@@ -1,3 +1,7 @@
 import { HashMap } from '@esfx/collections-hashmap';
+import { HashMap as HashMap1 } from '@hashmap';
+import { HashMap as HashMap2 } from '@hashmap2';
 
 const map = new HashMap();
+const map1 = new HashMap1();
+const map2 = new HashMap2();
